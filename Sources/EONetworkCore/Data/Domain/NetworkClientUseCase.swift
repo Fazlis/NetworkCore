45e4c052a5ff0fe65e214088209bc93c6bee74ca
Iskandar <fazliddinov.i@eskhata.tj>
2024-01-16 12:57:48 +0500

    adding Logger

--- Sources/EONetworkCore/Data/Domain/NetworkClientUseCase.swift
+++ Sources/EONetworkCore/Data/Domain/NetworkClientUseCase.swift
@@ -9,57 +9,63 @@ public protocol NetworkClientUseCase: AnyObject {
 }
 
 public final class NetworkClient: NSObject, NetworkClientUseCase {
+    
     public func sendRequest<T: Codable>(request: NetworkRequest, completion: @escaping NetworkResponse<T>) where T: Codable {
-        print(request.urlString)
-        print(request.headers)
-        print(request.method)
-        print(request.parameters)
-        
-        guard let url = URL(string: request.urlString) else {
-            completion(.failure(.invalidURL))
-            return
+        do {
+            let urlRequest = try createURLRequest(from: request)
+            
+            let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(.requestFailed(error)))
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(.invalidResponse))
+                    return
+                }
+                
+                if (200..<300).contains(httpResponse.statusCode), let data = data {
+                    do {
+                        let decodedObject = try JSONDecoder().decode(T.self, from: data)
+                        completion(.success(decodedObject))
+                    } catch {
+                        Logger.printDecodingError(error: error)
+                        completion(.failure(.invalidData))
+                    }
+                } else {
+                    completion(.failure(.invalidResponse))
+                }
+            }
+            task.resume()
+            
+        } catch {
+            completion(.failure(.requestFailed(error)))
         }
+    }
 
+    private func createURLRequest(from request: NetworkRequest) throws -> URLRequest {
+        guard let url = URL(string: request.urlString) else {
+            throw NetworkError.invalidURL
+        }
+        
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = request.method
-
         
         request.headers?.forEach { key, value in
             urlRequest.setValue(value, forHTTPHeaderField: key)
         }
-
+        
         if let parameters = request.parameters {
             do {
                 urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters)
             } catch {
-                completion(.failure(.invalidData))
-                return
+                throw NetworkError.invalidData
             }
         }
-
-        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let error = error {
-                completion(.failure(.requestFailed(error)))
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(.invalidResponse))
-                return
-            }
-            
-            if (200..<300).contains(httpResponse.statusCode), let data = data {
-                do {
-                    let decodedObject = try JSONDecoder().decode(T.self, from: data)
-                    completion(.success(decodedObject))
-                } catch {
-                    Logger.printDecodingError(error: error)
-                    completion(.failure(.invalidData))
-                }
-            } else {
-                completion(.failure(.invalidResponse))
-            }
-        }
-        task.resume()
+        
+        Logger.printRequest(request: urlRequest)
+        
+        return urlRequest
     }
 }
